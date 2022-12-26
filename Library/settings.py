217abebab3a94458f1b8411c0bff55810af777b3
pr0pr09er <113778429+pr0pr09er@users.noreply.github.com>
2022-12-26 14:02:46 +0300

    authorization

--- Library/settings.py
+++ Library/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Book.apps.BookConfig'
+    'Book.apps.BookConfig',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
